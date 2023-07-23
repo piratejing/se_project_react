fb633656d7b63215c945925a4f89fb77aa507cba
Alan Shieh <piratejing41@gmail.com>
2023-07-23 14:55:03 -0700

    re added Sidebar js file

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import "../blocks/Sidebar.css";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+const Sidebar = ({ handleEditProfile, handleLogOut }) => {
+  const currentUser = useContext(CurrentUserContext);
+  const currentUserName = currentUser?.data?.name;
+  const currentUserAvatar = currentUser?.data?.avatar;
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__container">
+        <img className="sidebar__useravatar" src={currentUserAvatar} alt="avatar" />
+        <div className="sidebar__username">{currentUserName}</div>
+      </div>
+      <button type="button" className="sidebar__edit-profile-button" onClick={handleEditProfile}>
+        Change profile data
+      </button>
+      <button type="button" className="sidebar__edit-logout-button" onClick={handleLogOut}>
+        Log out
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
